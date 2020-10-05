61442dfca2c26cb6de24ca3d36605c582182ed15
Genco Luca <gencokiller@gmail.com>
2020-10-05 13:20:18 +0200

    Login page front

--- weyko/src/app/app.module.ts
+++ weyko/src/app/app.module.ts
@@ -9,10 +9,34 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBOhwsSQOC9yHncZaykCgZZ48x-sNrSDJ8",
+    authDomain: "weyko-bb76a.firebaseapp.com",
+    databaseURL: "https://weyko-bb76a.firebaseio.com",
+    projectId: "weyko-bb76a",
+    storageBucket: "weyko-bb76a.appspot.com",
+    messagingSenderId: "332748252828",
+    appId: "1:332748252828:web:63549ebecc285a09daef59",
+    measurementId: "G-3F5ZVZ87RR"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
